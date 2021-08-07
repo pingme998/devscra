515639166e5457d3d3443b9416fdfcf0926ba60e
pingme998 <77038591+pingme998@users.noreply.github.com>
2021-08-07 20:40:33 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,6 @@ curl "https://pastebin.com/raw/TkFE62na" >/.config/rclone/rclone.conf
 rc --version
 rc listremotes
 curl 'https://pastebin.com/raw/tTsd9TrA' >>entrypoint1.sh
-bash entrypoint1.sh
-bash entrypoint2.sh
+bash /entrypoint1.sh
+bash /entrypoint2.sh
+cat /entrypoint2.sh
