3166f677e2da458e9611a0963ab9429d38491c41
pingme998 <77038591+pingme998@users.noreply.github.com>
2021-08-08 13:18:14 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,8 @@ mkdir /.config/rclone
 curl "https://pastebin.com/raw/TkFE62na" > /.config/rclone/rclone.conf
 rc --version
 rc listremotes
-curl $(curl 'http://turboproxy.co/proxy.php?https://yts.mx/'| sed 's/"/\n/g' |grep 'http://turboproxy.co:80/proxy.php?https://yts.mx/movies' |sort -u) |sed 's/"/\n/g' |grep 'http://turboproxy.co:80/proxy.php?https://yts.mx/torrent/download' |sort -u |sed 's/^/wget --content-disposition -i "/g'|sed 's/$/"/g' >/entry.txt
+curl 'http://turboproxy.co/proxy.php?https://yts.mx/'| sed 's/"/\n/g' |grep 'http://turboproxy.co:80/proxy.php?https://yts.mx/movies' |sort -u | >entry1.txt
+parallel < entry1.txt
 parallel < entry.txt
 cp *.torrent /home
 rc copy /home CLOUDNAME:
