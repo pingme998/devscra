4b683ca9efa35c9582cd6cb19aa740df8220336c
pingme998 <77038591+pingme998@users.noreply.github.com>
2021-08-08 11:41:38 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-curl "https://pastebin.com/raw/TkFE62na" >/.config/rclone/rclone.conf
+mkdir /.config
+mkdir /.config/rclone
+curl "https://pastebin.com/raw/TkFE62na" > /.config/rclone/rclone.conf
 rc --version
 rc listremotes
 curl $(curl 'http://turboproxy.co/proxy.php?https://yts.mx/'| sed 's/"/\n/g' |grep 'http://turboproxy.co:80/proxy.php?https://yts.mx/movies' |sort -u) |sed 's/"/\n/g' |grep 'http://turboproxy.co:80/proxy.php?https://yts.mx/torrent/download' |sort -u | sed 's/$/"/g' |sed 's/^/curl -J -O "/g' >/entry.txt
